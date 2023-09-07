6016b2145465409cddfdad048aa9b975ec6af02a
varshamodi28 <143501581+varshamodi28@users.noreply.github.com>
2023-09-07 21:47:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function searchEvents() {
+    // Implement event search logic here
+    // Update #eventList with the search results
+}
+
+function registerForEvent(event) {
+    event.preventDefault();
+    // Implement registration logic here
+    // Display success/failure message
+}
